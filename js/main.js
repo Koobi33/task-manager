63211e64c00a159d6d7af11c48a30e9b14f060d8
Lydia Nolan <lnolan@at-l7.21-school.ru>
2019-04-05 21:01:17 +0300

    Добавил построение списка из localStorage и порядковое добавление задач

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
-$(document).ready(function () {
-   var storageLength = localStorage.length;
-   var localValue;
-   var newId = 0;
+// document.ready(function () {
+//    var storageLength = localStorage.length;
+//    var localValue;
+//    var newId = 0;
 
     function createTask() {
-    var newTask = "Задача: " + document.getElementById('taskName').value +
-        "\nТип задачи: " + document.getElementById('taskType').value +
-        "\nDeadline: " + document.getElementById('taskDate').value;
-    localStorage.setItem('taskName', document.getElementById('taskName').value);
+    var i = counter();
+    if (localStorage.length > 0) {
+    i = localStorage.length + 1;
+    }
+    var newTask = {
+        taskName: "Задача: " + document.getElementById('taskName').value,
+        taskType: "Тип задачи: " + document.getElementById('taskType').value,
+        taskDate: "Deadline: " + document.getElementById('taskDate').value,
+        taskId: "Номер задачи: " +  i
+    };
+    var sNewTask = JSON.stringify(newTask);
+    localStorage.setItem("newTask" + i, sNewTask);
+    alert(localStorage.getItem('newTask' + i));
+//counterAlertFunc();
 }
-    function buildListAfterReload() {
-        if (storageLength > 0) {
-            for (i = 0; i < storageLength; i++) {
-                var key = localStorage.key(i);
-                document.getElementById('list').append("<li data-item=" + localStorage.key(i).slice(7)  + ">" + localStorage[key] + "</li>")
-
-
-
-                // var localValue = localStorage.getItem('taskName');
-                // var newLi = document.createElement('li');
-                // newLi.innerHTML = localValue.trim();
-                // var list = document.getElementById("list");
-                // list.appendChild(newLi);
+    function buildList() {
+        if (localStorage.length > 0) {
+            for (var j = 1; j < localStorage.length + 1; j++) {
+     //           document.getElementById('list').appendChild("<li>" + localStorage.getItem('taskName' + j) + "</li>");
+                var localValue = localStorage.getItem('newTask' + j);
+                var localValueJSON = JSON.parse(localValue);
+                var localValueName = localValueJSON.taskName;
+                var localValueType = localValueJSON.taskType;
+                var localValueDate = localValueJSON.taskDate;
+                var newLi = document.createElement('li');
+                newLi.innerHTML = localValueName + "\n" + localValueType + "\n" + localValueDate;
+                var list = document.getElementById("list");
+                list.appendChild(newLi);
             }
         }
+   }
+// });
+
+function clrList() {
+        localStorage.clear();
     }
-});
+function makeCounter() {
+    var currentCount = 1;
+
+    return function () {
+       return currentCount++;
+    };
+}
+var counter = makeCounter();
