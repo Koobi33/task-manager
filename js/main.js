df2b17e145cc1f7771344aab61213e6dc076b77a
Lydia Nolan <lnolan@at-l7.21-school.ru>
2019-04-07 19:25:56 +0300

    Сделано удаление и добавление элементов, перечеркивание выполненных задач

--- js/main.js
+++ js/main.js
@@ -4,47 +4,61 @@
 //    var newId = 0;
 
     function createTask() {
-    var i = counter();
     if (localStorage.length > 0) {
-    i = localStorage.length + 1;
+        for (var i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            var lastItem = localStorage.getItem(key);
+        }
+        var lastItemJSON = JSON.parse(lastItem);
+        i = lastItemJSON.taskId + 1;
+
+    } else {
+        i = 1;
     }
     var newTask = {
         taskName: "Задача: " + document.getElementById('taskName').value,
         taskType: "Тип задачи: " + document.getElementById('taskType').value,
         taskDate: "Deadline: " + document.getElementById('taskDate').value,
-        taskId: "Номер задачи: " +  i
+        taskId: "" +  i
     };
     var sNewTask = JSON.stringify(newTask);
     localStorage.setItem("newTask" + i, sNewTask);
-    alert(localStorage.getItem('newTask' + i));
-//counterAlertFunc();
 }
-    function buildList() {
+function deleteLi(elem) {
+    elem.parentElement.remove();
+    localStorage.removeItem("newTask" + elem.parentElement.id)
+}
+
+function complete(elem) {
+    elem.className = (elem.className === "inProgress") ? "completed" : "inProgress";
+}
+
+function buildList() {
         if (localStorage.length > 0) {
-            for (var j = 1; j < localStorage.length + 1; j++) {
-     //           document.getElementById('list').appendChild("<li>" + localStorage.getItem('taskName' + j) + "</li>");
-                var localValue = localStorage.getItem('newTask' + j);
-                var localValueJSON = JSON.parse(localValue);
-                var localValueName = localValueJSON.taskName;
-                var localValueType = localValueJSON.taskType;
-                var localValueDate = localValueJSON.taskDate;
-                var newLi = document.createElement('li');
-                newLi.innerHTML = localValueName + "\n" + localValueType + "\n" + localValueDate;
-                var list = document.getElementById("list");
-                list.appendChild(newLi);
+            for (let j = 0; j < localStorage.length; j++) {
+                let key = localStorage.key(j);
+                let localValue = localStorage.getItem(key);
+                let localValueJSON = JSON.parse(localValue);
+                let localValueName = localValueJSON.taskName;
+                let localValueType = localValueJSON.taskType;
+                let localValueDate = localValueJSON.taskDate;
+                let localValueId = localValueJSON.taskId;
+               let newLi = localValueName + "\n" + localValueType + "\n" + localValueDate + "\n" ;
+              // document.getElementById("list").append("<li class='inProgress' " + "id='" + localValueId + "' onclick='complete(this)'><span onclick='deleteLi(this)'>X</span> " + newLi + "</li>");
+               $("#list").append("<li class='inProgress' " + "id='" + localValueId + "' onclick='complete(this)'><span onclick='deleteLi(this)'>X</span> " + newLi + "</li>")
             }
         }
    }
-// });
+
 
 function clrList() {
         localStorage.clear();
     }
-function makeCounter() {
-    var currentCount = 1;
-
-    return function () {
-       return currentCount++;
-    };
-}
-var counter = makeCounter();
+// function makeCounter() {
+//     var currentCount = 1;
+//
+//     return function () {
+//        return currentCount++;
+//     };
+// }
+// var counter = makeCounter();
