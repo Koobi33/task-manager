3018a8f911024dee88bbf0015651797541ba4899
Lydia Nolan <lnolan@vo-e3.21-school.ru>
2019-04-11 19:45:30 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -13,23 +13,82 @@ function createTask() {
         i = 1;
     }
     var newTask = {
-        taskName: "Задача: " + document.getElementById('taskName').value,
-        taskType: "Тип задачи: " + document.getElementById('taskType').value,
+        taskName: "" + document.getElementById('taskName').value,
+        taskType:  "Тип Задачи: " + document.getElementById('taskType').value,
         taskDate: "" + document.getElementById('taskDate').value,
-        taskId: "" +  i
+        taskId: i,
+        taskIsComplete: false,
     };
     var sNewTask = JSON.stringify(newTask);
     localStorage.setItem("newTask" + i, sNewTask);
 }
+
+
 function deleteLi(elem) {
     elem.parentElement.remove();
     localStorage.removeItem("newTask" + elem.parentElement.id)
 }
 
 function complete(elem) {
-    elem.className = (elem.className === "inProgress") ? "completed" : "inProgress";
+    if (elem.className === "inProgress") {
+        elem.className = "completed";
+        let isComplete = JSON.parse(localStorage.getItem("newTask" + elem.parentElement.id));
+        isComplete.taskIsComplete = true;
+        let strIsComplete = JSON.stringify(isComplete);
+        localStorage.setItem("newTask" + elem.parentElement.id, strIsComplete);
+    } else if (elem.className === "completed") {
+        elem.className = "inProgress";
+        let isComplete = JSON.parse(localStorage.getItem("newTask" + elem.parentElement.id));
+        isComplete.taskIsComplete = false;
+        let strIsComplete = JSON.stringify(isComplete);
+        localStorage.setItem("newTask" + elem.parentElement.id, strIsComplete);
+    }
 }
 
+function refactorTask(elemID) {
+    var refactored = {
+        taskName: "" + document.getElementById('taskNameRef').value,
+        taskType:  "Тип Задачи: " + document.getElementById('taskTypeRef').value,
+        taskDate: "" + document.getElementById('taskDateRef').value,
+        taskId: elemID,
+        taskIsComplete: false,
+    };
+    var strRefactored = JSON.stringify(refactored);
+    localStorage.setItem("newTask" + elemID, strRefactored);
+    location.reload();
+}
+
+function refactorLi(elem) {
+
+    var elementStorage = JSON.parse(localStorage.getItem("newTask" + elem.parentElement.id));
+    var div = document.createElement("div");
+    div.className = "refactor";
+    div.innerHTML = "<h1>Редактирование элемента " + elementStorage.taskName + "</h1>" + "<form><div class=\"form-row\">\n" +
+        "                        <div class=\"form-group col-md-6\">\n" +
+        "\n" +
+        "                            <label for=\"taskName\"></label><input class=\"form-control\" type=\"text\" value=\"" + elementStorage.taskName + "\" id=\"taskNameRef\" placeholder=\"Новая задача\" name=\"taskName\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"form-group col-md-6\">\n";
+    if (elementStorage.taskType === "Тип Задачи: Личные задачи"){
+        div.innerHTML += "<label for=\"taskType\"></label><select class=\"form-control\" id=\"taskTypeRef\" name=\"taskType\">" +
+            "                        <option selected='selected'>Личные задачи</option>\n" +
+            "                        <option>Рабочие задачи</option>\n" +
+            "                        </select>\n" +
+            "                        </div>\n";
+    }else if (elementStorage.taskType === "Тип Задачи: Рабочие задачи"){
+        div.innerHTML += "<label for=\"taskType\"></label><select class=\"form-control\" id=\"taskTypeRef\" name=\"taskType\">" +
+            "                        <option>Личные задачи</option>\n" +
+            "                        <option selected='selected'>Рабочие задачи</option>\n" +
+            "                        </select>\n" +
+            "                        </div>\n";
+    }
+         div.innerHTML += "                        <div class=\"form-group sm col-3\">\n" +
+        "                        <label for=\"taskDate\"></label> <input type=\"datetime-local\" value=\"" + elementStorage.taskDate + "\" id=\"taskDateRef\" name=\"taskDate\" min=\"2019-04-01T00:00\" max=\"2019-12-31T23:59\">\n" +
+        "                        </div>\n" +
+        "                        </div>\n" +
+        "                        <button type=\"submit\" value=\"Создать\" formmethod=\"post\" class=\"btn btn-primary\" onclick=\"refactorTask(" + elementStorage.taskId + ");\">Изменить</button></form>";
+    elem.parentElement.parentElement.appendChild(div);
+}
 
 function buildList() {
 
@@ -47,23 +106,29 @@ function buildList() {
             let localValueDateNorm = Date.parse(localValueJSON.taskDate) / 60;
             let diff = currentDate - localValueDateNorm;
             diff = Math.round(diff / 60);
+
             let newLi = localValueName + "\n" + localValueType + "\n" + localValueDate + "\n";
-            if (localValueJSON.taskType === "Тип задачи: Личные задачи") {
-                $("#personList").append("<li class='inProgress' " + "id='" + localValueId + "' onclick='complete(this)'><span onclick='deleteLi(this)'>X</span> " + newLi + "</li>")
+            if (localValueJSON.taskType === "Тип Задачи: Личные задачи") {
+                $("#personList").append("<li class='inProgress' " + "id='" + localValueId + "'><span onclick='deleteLi(this)'>X</span> <p class='inProgress' onclick='complete(this)'>" + newLi + "</p><span onclick='refactorLi(this)'>Редактировать</span> </li>");
+                if (localValueJSON.taskIsComplete === true)
+                    document.getElementById(localValueId).firstElementChild.nextElementSibling.className = "completed";
                 if (localValueDateNorm < currentDate)
                     document.getElementById(localValueId).className += " red";
                 if (diff < 0 && diff > -24000)
                     document.getElementById(localValueId).className += " yellow";
             }
-            if (localValueJSON.taskType === "Тип задачи: Рабочие задачи") {
-                $("#workList").append("<li class='inProgress' " + "id='" + localValueId + "' onclick='complete(this)'><span onclick='deleteLi(this)'>X</span> " + newLi + "</li>")
-                if (localValueDateNorm < currentDate) {
+            if (localValueJSON.taskType === "Тип Задачи: Рабочие задачи") {
+                $("#workList").append("<li class='inProgress' " + "id='" + localValueId + "'><span onclick='deleteLi(this)'>X</span> <p class='inProgress' onclick='complete(this)'>" + newLi + "</p><span onclick='refactorLi(this)'>Редактировать</span> </li>");
+                if (localValueJSON.taskIsComplete === true)
+                    document.getElementById(localValueId).firstElementChild.nextElementSibling.className = "completed";
+                if (localValueDateNorm < currentDate)
                     document.getElementById(localValueId).className += " red";
-                }
                 if (diff < 0 && diff > -24000)
                     document.getElementById(localValueId).className += " yellow";
             }
-            $("#list").append("<li class='inProgress' " + "id='" + localValueId + "' onclick='complete(this)'><span onclick='deleteLi(this)'>X</span> " + newLi + "</li>")
+            $("#list").append("<li class='inProgress' " + "id='" + localValueId + "'><span onclick='deleteLi(this)'>X</span> <p class='inProgress' onclick='complete(this)'>" + newLi + "</p><span onclick='refactorLi(this)'>Редактировать</span> </li>");
+            if (localValueJSON.taskIsComplete === true)
+                document.getElementById(localValueId).firstElementChild.nextElementSibling.className = "completed";
             if (localValueDateNorm < currentDate)
                 document.getElementById(localValueId).className += " red";
             if (diff < 0 && diff > -24000)
